31007cc8122fa071ab2a7a9a983552459122a9ab
Ethan Liu <ethanliu818@gmail.com>
2024-01-22 17:16:33 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import "./App.css";
+import Timer from "./Timer";
+import Settings from "./Settings";
+import { useState } from "react";
+import SettingsContext from "./SettingsContext";
+
+function App() {
+  const [showSettings, setShowSettings] = useState(false);
+  const [workMinutes, setWorkMinutes] = useState(45);
+  const [breakMinutes, setBreakMinutes] = useState(15);
+
+  return (
+    <main>
+      <SettingsContext.Provider
+        value={{
+          workMinutes,
+          breakMinutes,
+          setWorkMinutes,
+          setBreakMinutes,
+          setShowSettings,
+        }}
+      >
+        {showSettings ? <Settings /> : <Timer />}
+      </SettingsContext.Provider>
+    </main>
+  );
+}
+
+export default App;
